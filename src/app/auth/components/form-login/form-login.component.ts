a64b411019a41af11f73cbf6ee191a08f5f31971
cristopher <crisotopher.saraos@gmail.com>
2021-11-11 22:49:40 -0300

    login and add toast

--- src/app/auth/components/form-login/form-login.component.ts
+++ src/app/auth/components/form-login/form-login.component.ts
@@ -1,28 +1,54 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-form-login',
-  templateUrl: './form-login.component.html',
-  styles: [
-  ]
+	selector: 'app-form-login',
+	templateUrl: './form-login.component.html',
+	styles: [],
 })
 export class FormLoginComponent implements OnInit {
+	constructor(private fb: FormBuilder, private authService: AuthService) {}
 
-  constructor( private fb:FormBuilder) { }
+	myFormLogin!: FormGroup;
 
-  myFormLogin!:FormGroup;
+	ngOnInit(): void {
+		this.myFormLogin = this.fb.group({
+			userName: ['', Validators.required],
+			password: ['', [Validators.required, Validators.minLength(6)]],
+		});
+	}
 
-  ngOnInit(): void {
+	login() {
+		const username = this.myFormLogin.controls['userName'].value;
+		const password = this.myFormLogin.controls['password'].value;
+		this.authService.login(username, password).subscribe(
+			(res) => {
+                if(res == true){
+                  this.modal('Access granted','success')
+                }else{
+                    this.modal(res.toString(),'warning')
+                }       
+            },
+			(err: HttpErrorResponse) => {
+                this.modal(err.message,'warning')
+			}
+		);
+	}
 
-    this.myFormLogin = this.fb.group({       
-        userName    : ['', Validators.required],
-        password : ['', [
-                Validators.required,
-                Validators.minLength(6)
-            ]
-        ],
-    });
-  }
 
+    modal(title:string, icon:any ){
+          Swal.fire({
+            title,
+            icon,
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+    }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  URLBASE:'http://161.35.140.236:9005/api'
 };
