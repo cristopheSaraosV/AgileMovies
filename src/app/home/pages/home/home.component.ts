24851e1539986ad15fb64e70960f106a262a94a1
cristopher <crisotopher.saraos@gmail.com>
2021-11-12 14:57:17 -0300

    add load Movie list

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -3,30 +3,39 @@ import { Datum } from '../../interfaces/movieNowPlaying.interface';
 import { MovieService } from '../../services/movie.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: [
-  ]
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styles: [],
 })
 export class HomeComponent implements OnInit {
+	constructor(private movieService: MovieService) {}
 
-  constructor(
-    private movieService: MovieService
+	listMostPopularMovies: Datum[] = [];
 
-  ) { }
+	page: number = 1;
+	ngOnInit(): void {
+		this.getMostPopularMovies();
+	}
 
-  listMostPopularMovies: Datum[] = [];
+	getMostPopularMovies() {
+		this.movieService.getMostPopularMovies(this.page).subscribe(
+			(res) => {
+				this.listMostPopularMovies = res;
+			},
+			(err) => {}
+		);
+	}
 
-  ngOnInit(): void {
-        this.getMostPopularMovies(); 
-  }
-
-
-  getMostPopularMovies(){
-    this.movieService.getMostPopularMovies(1).subscribe( res => {
-        this.listMostPopularMovies = res;
-    },err => {
-       
-    });
-  }
+	loadMoreMovies() {
+		this.movieService.getMostPopularMovies(this.page+1).subscribe(
+			(res) => {
+                this.page = this.page+1 
+                res.forEach((movie:any) => {
+                    this.listMostPopularMovies.push(movie);
+                    
+                });
+			},
+			(err) => {}
+		);
+	}
 }
